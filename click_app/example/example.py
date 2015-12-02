eca13c3dcbe02c16a8070ab6c577bdb4a3b900cb
Peter Jerold Leslie <jeroldleslie@gmail.com>
2015-12-03 01:21:38 +0530

    Update example.py

--- click_app/example/example.py
+++ click_app/example/example.py
@@ -7,4 +7,8 @@ Created on 02-Dec-2015
 import click_app
 
 if __name__ == '__main__':
+    #This will call click_app.run_string function in click_app
+    #Arguments should be array of options that we need to pass
+    #for example, if you need to print click help, call click_app.run_string with args like 
+    #click_app.run_string(["--help"])
     click_app.run_string(["-s", "Nvent"])
